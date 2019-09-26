a0846d89066a1b05268f1bb51c5e30427cd58793
Zisheng Huang <hellohuangzisheng@outlook.com>
2019-09-26 20:04:35 +0800

    Update buf.h
    结构体buf的定义，是双链表节点

--- buf.h
+++ buf.h
@@ -3,11 +3,11 @@ struct buf {
   uint dev;
   uint blockno;
   struct sleeplock lock;
-  uint refcnt;
-  struct buf *prev; // LRU cache list
+  uint refcnt;      //引用次数
+  struct buf *prev; // LRU cache list 最近最少使用的缓存列表
   struct buf *next;
   struct buf *qnext; // disk queue
-  uchar data[BSIZE];
+  uchar data[BSIZE]; //存放的数据
 };
 #define B_VALID 0x2  // buffer has been read from disk
 #define B_DIRTY 0x4  // buffer needs to be written to disk
