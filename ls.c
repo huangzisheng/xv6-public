589424725fa903342f2eace73b532d32237c7d73
Zisheng Huang <hellohuangzisheng@outlook.com>
2019-10-15 15:25:28 +0800

    Update ls.c

--- ls.c
+++ ls.c
@@ -3,6 +3,7 @@
 #include "user.h"
 #include "fs.h"
 
+//path是指定文件的路径，fmtname将返回指向文件名字符串的指针
 char*
 fmtname(char *path)
 {
@@ -10,6 +11,7 @@ fmtname(char *path)
   char *p;
 
   // Find first character after last slash.
+  //从后往前遍历，找到字符“/”
   for(p=path+strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
@@ -35,14 +37,17 @@ ls(char *path)
     return;
   }
 
+  //根据文件描述符获取文件状态，文件状态存放在结构体st里
   if(fstat(fd, &st) < 0){
     printf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
+  //接下来根据文件的类型：文件或目录文件 进行不同的操作
   switch(st.type){
   case T_FILE:
+    //输出该文件对应的文件名、文件类型、文件的i节点、文件的大小
     printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
     break;
 
@@ -54,15 +59,17 @@ ls(char *path)
     strcpy(buf, path);
     p = buf+strlen(buf);
     *p++ = '/';
+    //此时fd对应的文件是目录文件，循环从目录文件中读取每一个结构体dirent
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
+      if(de.inum == 0)    //索引节点号如果是0的话跳过？
         continue;
-      memmove(p, de.name, DIRSIZ);
+      memmove(p, de.name, DIRSIZ);//p指向的是buf后的第一个位置，此操作将在当前目录文件里遍历到的文件的文件名追加到buf后面
       p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
+      if(stat(buf, &st) < 0){     //获取对应文件的状态
         printf(1, "ls: cannot stat %s\n", buf);
         continue;
       }
+      //输出该文件对应的文件名、文件类型、文件的i节点、文件的大小
       printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
     }
     break;
@@ -75,6 +82,7 @@ main(int argc, char *argv[])
 {
   int i;
 
+  //如果ls的参数不够，则表示对当前目录执行ls操作
   if(argc < 2){
     ls(".");
     exit();
